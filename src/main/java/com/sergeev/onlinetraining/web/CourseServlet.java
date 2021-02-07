be3ccfd0ece120f7b7618dc96d737cbe59400a05
Evgeniy <sea2512@yandex.ru>
2021-02-07 11:01:18 +0300

    Hello world commit

---
+++ src/main/java/com/sergeev/onlinetraining/web/CourseServlet.java
@@ -0,0 +1,22 @@
+package com.sergeev.onlinetraining.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author sea
+ */
+public class CourseServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("course.jsp").forward(req, resp);
+    }
+
+}
